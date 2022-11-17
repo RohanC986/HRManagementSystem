2f938373b870b0ff86b5d7c58a6db3b0f0758f98
patilmayur14120 <mayur.patil@wonderbiz.in>
2022-11-17 16:10:32 +0530

    Mayur Patil 17 Nov

--- EmployeeManagementSystemCore/ViewModels/LoginViewModel.cs
+++ EmployeeManagementSystemCore/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,15 @@ namespace EmployeeManagementSystemCore.ViewModels
     public class LoginViewModel
     {
         public  int? EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Designation Name required")]
+        [RegularExpression("^\\S+@\\S+\\.\\S+$", ErrorMessage = "E-mail id is not valid")]
+
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Designation Name required")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,12}$", ErrorMessage = "Password is not valid")]
+
         public string Password { get; set; }
         public int RoleId { get; set; }
         public int IsActive { get; set; }   
